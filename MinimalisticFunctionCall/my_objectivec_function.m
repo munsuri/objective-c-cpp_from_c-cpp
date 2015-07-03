a360424955b9bae92c344102e25211de565c7b2c
Iván Munsuri Ibáñez <munsuri@gmail.com>
2015-07-03 15:54:39 +0200

    First minimalistic example of Objective C code called from C.

---
+++ MinimalisticFunctionCall/my_objectivec_function.m
@@ -0,0 +1,32 @@
+/*The MIT License (MIT)
+
+Copyright (c) <2015> <Iván Munsuri Ibañez>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
+#import "my_objectivec_interface.h"
+#import <Foundation/Foundation.h> //NSLog
+
+// C "trampoline" function to invoke Objective-C method
+int my_objectivec_function (void *a, char *b, int c){
+    // this is Objective-C world
+    NSLog(@"Hello World, a %p, b %s, c %d", a, b, c);
+    return 69;
+}
